31d1d410c06255dac5c0339c68e98796291863e7
rajatamatya <rajatamatya20@gmail.com>
2018-03-21 20:37:28 +0545

    Updated wp support folders

---
+++ wp-snapshots/tmp/20180321_legendsoftomorrow_acbf3522c2699a481616180321141130_database.sql
@@ -0,0 +1,4 @@
+
+
+/* Duplicator WordPress Timestamp: 2018-03-21 14:11:45*/
+/* DUPLICATOR_MYSQLDUMP_EOF */
---
+++ wp-snapshots/tmp/legendsoftomorrow_55ac0532a74a7d063977180321143353_database.sql
@@ -0,0 +1,4 @@
+
+
+/* Duplicator WordPress Timestamp: 2018-03-21 14:34:40*/
+/* DUPLICATOR_MYSQLDUMP_EOF */
